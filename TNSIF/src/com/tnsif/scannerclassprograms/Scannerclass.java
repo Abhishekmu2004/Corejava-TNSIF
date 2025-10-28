cdcf4551d3f20db5f4766d15b22dba0f93b5f09c
MONIKA <MONIKA@walton>
2025-10-28 21:38:50 +0530

    second

---
+++ TNSIF/src/com/tnsif/scannerclassprograms/Scannerclass.java
@@ -0,0 +1,18 @@
+package com.tnsif.scannerclassprograms;
+import java.util.Scanner;
+
+//demo for scanner class
+public class Scannerclass {
+
+	public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter the id");
+	int	eid=sc.nextInt();
+	System.out.println("id number:"+ eid);
+	
+	System.out.println("enter the name");
+	String name=sc.next();
+	System.out.println("name of the emp :" +name);
+	}
+}

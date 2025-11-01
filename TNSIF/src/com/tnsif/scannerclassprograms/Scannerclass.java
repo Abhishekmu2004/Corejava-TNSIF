05de1c3dc828f4ee1f953444fc730795bf6cf4ce
MONIKA <MONIKA@walton>
2025-11-01 23:10:29 +0530

    second

--- TNSIF/src/com/tnsif/scannerclassprograms/Scannerclass.java
+++ TNSIF/src/com/tnsif/scannerclassprograms/Scannerclass.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class Scannerclass {
 
 	public static void main(String[] args) {
-Scanner sc=new Scanner(System.in);
+    Scanner sc=new Scanner(System.in);
 		
-		System.out.println("enter the id");
+	System.out.println("enter the id");
 	int	eid=sc.nextInt();
 	System.out.println("id number:"+ eid);
-	
 	System.out.println("enter the name");
 	String name=sc.next();
 	System.out.println("name of the emp :" +name);

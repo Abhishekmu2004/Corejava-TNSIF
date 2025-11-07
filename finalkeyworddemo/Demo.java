e617cd2848921232b7bca312d11f0cb804e65a1c
Abhishekmu2004 <abhigowdamsd@gmail.com>
2025-11-07 22:19:15 +0530

    Add files via upload

---
+++ finalkeyworddemo/Demo.java
@@ -0,0 +1,12 @@
+package com.tnsif.finalkeyworddemo;
+
+public class Demo extends Finaldemo{
+	@Override
+	 void display() {
+		System.out.println("welcome to python");
+	}
+public static void main(String[] args) {
+	Demo obj = new Demo();
+	obj.display();
+}
+}
---
+++ finalkeyworddemo/Finaldemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.finalkeyworddemo;
+//demo for final keyword
+//variable
+//method
+//class
+public  class Finaldemo {
+  static int a=8;  // u cant change value
+ 
+  void display() {
+	 System.out.println("welcome to java");
+ }
+	public static void main(String[] args) {
+	    Finaldemo obj = new Finaldemo();
+	    obj.display();
+}
+}
